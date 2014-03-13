e1195a24d99195c2481d371c50a4a60d8d9803cc
mateuszek <mateusz.dargacz@internetprofil.pl>
2014-03-14 00:37:09 +0100

    pre-translation

--- static/js/contact_form.js
+++ static/js/contact_form.js
@@ -1,4 +1,6 @@
+    var error_message;
     function isValidEmailAddress(emailAddress) {
+
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(emailAddress);
     }
@@ -22,26 +24,26 @@
         if(field =='message'){
             input = $('textarea[name=message]').val();
             if (input == ""){
-                error = { message : "To pole jest wymagane." }}
+                error = { message : error_message }}
             else{ error = { message : 0 }}
         }else{
             if (field =='email'){
                 input = $("input[name="+field+"]").val();
                 if (!isValidEmailAddress(input)){
-                    error = {email : "To pole jest wymagane."}}
+                    error = {email : error_message}}
                 else error = { email : 0  }
             }else{ if ((field =='name')){
                 input = $("input[name="+field+"]").val();
 
                 if (input == ""){
-                    error = { name: "To pole jest wymagane." }}
+                    error = { name: error_message }}
                 else{ error = { name : 0 }}
 
             } else{
                 if(field =='phone'){
                     input = $("input[name="+field+"]").val();
                     if (input == ""){
-                    error = { phone: "To pole jest wymagane." }}
+                    error = { phone: error_message }}
                     else{ phone = { name : 0 }}
                     }
                 }
@@ -65,6 +67,15 @@
 
     $( "#contact-form" ).submit(function( event ) {
         event.preventDefault();
+        var language = location.pathname.split("/")[1];
+
+        if  (language === "pl") error_message = "To pole jest wymagane.";
+        else if (language === "de") error_message = "Dieses Feld ist erforderlich.";
+             else if (language === "en") error_message = "This field is required.";
+                  else if (language === "ru") error_message = "Это поле обязательно.";
+
+
+        alert(language);
         var formFields = ["name","email","phone","message"];
         var name  = $("input[name=name]").val();
         var email = $("input[name=email]").val();
